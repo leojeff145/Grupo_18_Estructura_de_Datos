95425fec611815b28500c8476fd2082de3f274e3
LEONARDO OBANDO <jlobando2@espe.edu.ec>
2022-11-30 07:52:36 -0500

    bien

--- Primer Parcial/Rol_Pagos/Rol_Pagos/main.cpp
+++ Primer Parcial/Rol_Pagos/Rol_Pagos/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include "Menu.h"
 
+/*********
+ * UFA-ESPE
+ * ALUMNOS:  Leonardo Obando, William Leon
+ * FECHA : Lunes, 30 de noviembre de 2022
+ * Enunciado del problema: Proyecto
+ * Nivel: TERCERO NRC: 7999
+ * Git:https://github.com/leojeff145/Grupo_20_Estructura_de_Datos
+ *********/
+
 using namespace std;
 
 int main() {
